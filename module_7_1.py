884b66d21d44351cee4b529551c6b76ddd202d84
mik-305 <mik-305@bk.ru>
2024-11-08 16:22:43 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -1,25 +1,50 @@
 import io
 from pprint import pprint
 class Product:
+    global products
+    products = ''
     def __init__(self, name, weight, category):
         self.name = name
         self.weight = str(weight)
         self.category = category
     def __str__(self):
-        return self.name + ', ' + self.weight + ', ' + self.category
+        products = self.name+', '+ self.weight+', ' +self.category
+        return products
 
-class Shop:
+class Shop():
     def get_products(self):
         file = open('products.txt', 'r', encoding='utf-8')
+        print(file)
         pprint(file.read())
         file.close()
+        #return print(zapas)
+    def add(self, *products):               # Добавление продуктов в файл
+        file = open('products.txt', 'a', encoding='utf-8')
+        file.write('Новый текст\n')
+        #rint(self.name)
+        print('!!! ---', products[0])
+        #file.write('!!!', Product(self.name, self.weight, self.category),'\n')
+        file.write('!!!'+ str(products[0])+'\n')
+        #file.write(Product.__str__(self.name),'\n')
+        #print(Product(self.name, self.weight, self.category))
+        #print(Product.__str__(self))
+        file.close()
+
+
+
+
+
 
 s1 = Shop()
-print(s1.get_products())
 
 p1 = Product('Potato', 50.5, 'Vegetables')
-print(str(p1))
+
 p2 = Product('Spaghetti', 3.4, 'Groceries')
-print(str(p2))
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p1)
+print(p2)
+print(p3)
+s1.add(p1, p2, p3)
 
 
