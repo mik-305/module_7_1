af1db4deaa8663df48c069382b1ac53b4bdeb191
mik-305 <mik-305@bk.ru>
2024-11-09 19:46:21 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -14,37 +14,30 @@ class Product:
 class Shop():
     def get_products(self):
         file = open('products.txt', 'r', encoding='utf-8')
-        print(file)
-        pprint(file.read())
+        soder_file = file.read()                            # Содержимое файла
         file.close()
-        #return print(zapas)
-    def add(self, *products):               # Добавление продуктов в файл
+        return soder_file
+    def add(self, *products):                               # Добавление продуктов в файл если их там нет
         file = open('products.txt', 'a', encoding='utf-8')
-        file.write('Новый текст\n')
-        #rint(self.name)
-        print('!!! ---', products[0])
-        #file.write('!!!', Product(self.name, self.weight, self.category),'\n')
-        file.write('!!!'+ str(products[0])+'\n')
-        #file.write(Product.__str__(self.name),'\n')
-        #print(Product(self.name, self.weight, self.category))
-        #print(Product.__str__(self))
+        in_magazin = self.get_products()
+        i = 0
+        while i < len(products):
+            if products[i].name in in_magazin:
+                print(f'Продукт {products[i]} уже есть в магазине')
+            else:
+                file.write(str(products[i]) + '\n')
+            i += 1
         file.close()
-
-
-
-
-
-
+#--------------------------------------------------------------------------------------------------------
 s1 = Shop()
-
 p1 = Product('Potato', 50.5, 'Vegetables')
-
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p1)
-print(p2)
-print(p3)
+print(p2) # __str__
+
 s1.add(p1, p2, p3)
+
+print(s1.get_products())
 
 
